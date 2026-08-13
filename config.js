ef9602bf129ed9d3de5ea47f6f57a93fe2e6d75d
yasiyrshehu-byte <yasiyrshehu@gmail.com>
2026-08-13 21:40:03 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.TW_CONFIG = {
   // Replace this with your deployed HTTPS backend URL.
   // Example: https://ticketwaves-api.onrender.com/api
-  API_BASE_URL: "https://YOUR-RENDER-BACKEND.onrender.com/api",
+  API_BASE_URL: "https://ticketwaves-backend-1.onrender.com/api",
   SUPPORT_EMAIL: "ticketwavesofficial@gmail.com",
   SITE_NAME: "TicketWAVES"
 };
